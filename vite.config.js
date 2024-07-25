0c974931ed8194e83689244b9383902a86743543
Bastian Cordova <cordova.bas@gmail.com>
2024-07-25 04:55:39 -0400

    holita

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: 'src/index.jsx', // O 'src/index.js' si no usas TypeScript
+      name: 'ina-health-design',
+    },
+    rollupOptions: {
+      // Asegúrate de que los paquetes no se incluyan en el bundle final
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
